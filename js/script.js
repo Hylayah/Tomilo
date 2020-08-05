76c3d47c97fd1d7882a19c5f2cd1a88a8f1e7799
Artur <artur.chrapowicz@gmail.com>
2020-08-05 13:22:12 +0200

    demo finished

--- js/script.js
+++ js/script.js
@@ -12,6 +12,14 @@ let section3 = document.getElementById('section-3');
 let section4 = document.getElementById('section-4');
 let section5 = document.getElementById('section-5');
 
+let span1 = document.getElementById('section-span-1');
+let span2 = document.getElementById('section-span-2');
+let span3 = document.getElementById('section-span-3');
+let span4 = document.getElementById('section-span-4');
+let span5 = document.getElementById('section-span-5');
+
+let img = document.getElementById('section-img');
+
 let sections = [section1, section2, section3, section4, section5];
 
 for(var i = 0; i < sections.length; i++){
@@ -23,20 +31,25 @@ for(var i = 0; i < sections.length; i++){
         this.classList.add('active');
 
         if(this == section1) {
-            title.innerHTML = 'Sekcja 1';
-            text.innerHTML = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis aspernatur maiores tenetur, obcaecati corrupti nostrum eius. Nostrum possimus recusandae molestias illum temporibus dolorem suscipit, id accusantium animi quasi iste facere deleniti adipisci cumque beatae delectus numquam voluptate eum. Libero quae perspiciatis ab obcaecati culpa rerum minus fugiat recusandae ducimus rem! Id accusantium animi quasi iste facere deleniti adipisci cumque beatae delectus numquam voluptate eum. Libero quae perspiciatis ab obcaecati culpa rerum minus fugiat recusandae ducimus rem! Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus facere eum accusantium non harum ex ducimus, libero nihil animi optio maxime';
+            title.innerHTML = span1.innerHTML;
+            img.src = '../img/doctor.png';
+            text.innerHTML = 'ECG monitors and writers should meet safety requirements in regards to electrical safety. Maintenance must be performed to ensure functionality and safety for the different modules and possible wearing. Additionally, electrical safety tests, including earth leakage and connectivity, are also needed to ensure patient safety. Maintenance will be aligned with service manuals from manufacturers and includes: Visual check of buttons, display, chassis, Test of ECG leads, Battery test, Electrical safety test, General cleaning of instrument, Technical aids.';
         } else if(this == section2){
-            title.innerHTML = 'Sekcja 2';
-            text.innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+            title.innerHTML = span2.innerHTML;
+            img.src = '../img/scan.png';
+            text.innerHTML = 'Patient monitor service is important to ensure patient and user safety. During maintenance, the patient monitor is functionally tested and an electrical safety test is performed according to standards. Medical Equipment Service Paweł Tomiłop is an authorized service organization for several patient monitor manufacturers. The B105 and B125 patient monitors deliver portable monitoring with multi-parameters featuring alarms and physiological data points for adult and pediatric patients. Used as a stand-alone device or interfaced to a network of devices, they give you the added versatility you need across the care continuum.';
         } else if(this == section3){
-            title.innerHTML = 'Sekcja 3';
-            text.innerHTML = 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est.';
+            title.innerHTML = span3.innerHTML;
+            img.src = '../img/flask.png';
+            text.innerHTML = 'Respirators needs to be serviced according to manufacturer recommendations to ensure patient safety. Medical Equipment Service Paweł Tomiłop is an authorized service organization for several Respirator manufacturers. Preventive maintenance of a respirator includes, for example: <ul><li>Visual inspection</li> <li>Central gas measurements</li> <li>Internal pressure regulation</li></ul>';
         } else if(this == section4){
-            title.innerHTML = 'Sekcja 4';
-            text.innerHTML = 'But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example.';
+            title.innerHTML = span4.innerHTML;
+            img.src = '../img/rubber-gloves.png';
+            text.innerHTML = 'Laser surgery equipment needs to be serviced annually according to manufacturer instructions. It is important to ensure that the laser unit is working as the manufacturer requires. Medical Equipment Service Paweł Tomiłop is an authorized service organization for several laser surgery equipment manufacturers. aser surgery is a type of surgery that uses a laser (in contrast to using a scalpel) to cut tissue. Examples include the use of a laser scalpel in otherwise conventional surgery, and soft-tissue laser surgery, in which the laser beam vaporizes soft tissue with high water content. Laser surgery is commonly used on the eye. Techniques used include LASIK, which is used to correct near and far-sightedness in vision, and photorefractive keratectomy, a procedure which permanently reshapes the cornea using an excimer laser to remove a small amount of the human tissue.';
         } else if(this == section5){
-            title.innerHTML = 'Sekcja 5';
-            text.innerHTML = 'On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will.';
+            title.innerHTML = span5.innerHTML;
+            img.src = '../img/first-aid-kit.png';
+            text.innerHTML = 'In regard to medical device safety and performance, it is important that infusion pumps are regularly tested. This ensures that they are functioning to the manufacturer’s specifications and are within clinical and environmental expectations. Medical Equipment Service Paweł Tomiłop is an authorized service organization for several infusion pump manufacturers. Preventive maintenance of infusion pump includes, for example:    Visual check – chassis, labels, etc., Internal check – plugs and liquid, Test of all buttons, Verify date & time, Test of hose to line, Check drop-counter, Open door alarm  , Delivered volume test, Occlusion test, Battery test, Electrical safety test, General cleaning of instrument';
         }
     })
 }
